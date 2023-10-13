100de44425ef911f3cde8e53d9e448aab75e71b9
DuhaVx <147029284+DuhaVx@users.noreply.github.com>
2023-10-13 10:40:23 +0300

    Add files via upload

---
+++ Ploshad.js
@@ -0,0 +1,5 @@
+const dlinna1 = 5;
+const dlinna2 = 4;
+const dlinna3 = 3;
+const ploshad= 0.5*(dlinna1 + dlinna2 + dlinna3);
+console.log(Math.sqrt(ploshad*(ploshad-dlinna1)*(ploshad-dlinna2)*(ploshad-dlinna3)));
